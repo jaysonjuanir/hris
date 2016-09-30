b24d0d76c8113b59d561b2c7d0f54eb5ab5597cc
Jayson Juanir <jaysonjuanir>
2016-09-29 21:41:47 -0800

    employee.sql updated

--- infra/src/main/resources/employee.sql
+++ infra/src/main/resources/employee.sql
@@ -12,6 +12,16 @@ CREATE TABLE employee (
     city varchar(250),
     CONSTRAINT employee_pkey PRIMARY KEY (id)
 );
+DROP TABLE if EXISTS account CASCADE;
+CREATE TABLE account
+(
+  id bigserial NOT NULL,
+  username character varying(250),
+  password character varying(250),
+  lvl bigint,
+  employeeid bigint,
+  CONSTRAINT account_pkey PRIMARY KEY (id)
+);
 
 DROP TABLE if EXISTS messages CASCADE;
 CREATE TABLE messages (
@@ -31,3 +41,20 @@ CREATE TABLE actions (
 	employeeId bigint NOT NULL,
 	CONSTRAINT actions_pkey PRIMARY KEY (id)
 );
+
+DROP TABLE if EXISTS bulletin CASCADE;
+CREATE TABLE bulletin (
+    id bigserial NOT NULL,
+    message character varying(250),
+    bulletintype character varying(250),
+	bulletinsubject character varying(250),
+    employeename character varying(250),
+	CONSTRAINT bulletin_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO employee VALUES (1, 'Manager','Manager','Manager', '09261127796',1,'Manager', 'StreetNumber' , 'Barangay', 'City');
+INSERT INTO account VALUES (1, 'manager', '123', 2 , 1);
+INSERT INTO bulletin VALUES (1, 'test this bulletin', 'announcement', 'this subject' , 'Name Here');
+INSERT INTO bulletin VALUES (2, 'test this update', 'update', 'this subject', 'Name Here');
+INSERT INTO bulletin VALUES (3, 'test this news', 'news', 'this subject', 'Name Here');
+INSERT INTO bulletin VALUES (4, 'test this news 2', 'news', 'this subject', 'Name Here');
